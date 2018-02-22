fcff14f464baf3d6e03a7734cea0510b77489a1a
Nitish <nitishg@bu.edu>
2018-02-22 17:59:56 -0500

    Delete encoder.py

--- encoder.py
+++
@@ -1,7 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-#Basically Encoder is an interface, we cannot create an instance of it, only use it as a superclass
-class Encoder(metaclass=ABCMeta):
-    @abstractmethod
-    def generate(self, diagram):
-        """ Generate a output from the supplied diagram"""
