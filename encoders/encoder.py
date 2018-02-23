948bc5448fb806ef85ba99b031cc8ce29cee917d
Nitish <32949680+nishgaddam@users.noreply.github.com>
2018-02-22 19:19:43 -0500

    adding more comments

--- encoders/encoder.py
+++ encoders/encoder.py
@@ -1,7 +1,10 @@
 from abc import ABCMeta, abstractmethod
-
-#Basically Encoder is an interface, we cannot create an instance of it, only use it as a superclass
+#abc - abstract base class
+#Basically Encoder class is an interface, we cannot create an instance of it, only use it as a superclass
+#AN interface is a template that tells your class how to behave
 class Encoder(metaclass=ABCMeta):
     @abstractmethod
     def generate(self, diagram):
         """ Generate a output from the supplied diagram"""
+
+#By writing the @abstractmethod we are instructing the preety_print, source_code and xmi to compulsorily require a generate class         
